50d6899a460f7ca4b5b7e93f6ecb20889ede8788
王涛 <wang.tao@renren-inc.com>
2013-08-15 23:24:41 +0800

    create the project.

---
+++ ToZhao/ToZhao/TZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TZAppDelegate.h
+//  ToZhao
+//
+//  Created by wangtao on 13-8-15.
+//  Copyright (c) 2013年 wangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToZhao/ToZhao/TZViewController.h
@@ -0,0 +1,13 @@
+//
+//  TZViewController.h
+//  ToZhao
+//
+//  Created by wangtao on 13-8-15.
+//  Copyright (c) 2013年 wangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TZViewController : UIViewController
+
+@end
